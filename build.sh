158a0e0cc8d87b85401ba1edeab851bcb644850d
Kyle West <kyle@familysearch.org>
2018-06-22 08:31:38 -0600

    [MINIFICATION] working, and exports to .min.css and .html modules

--- build.sh
+++ build.sh
@@ -1,7 +1,33 @@
-pre="<dom-module id='element-skeleton'><template><style>"
-post="</style></template></dom-module>"
-content=$(sed -e ':a' -e 'N' -e '$!ba' -e 's/\n/ /g'  element-skeleton.css | sed -e 's/  */ /g')
+build_components () {
+  # Minify the CSS
+  content=$(sed -e ':a' -e 'N' -e '$!ba' -e 's/\n//g' element-skeleton.css | sed -e 's/  */ /g')
+  content=$(echo "$content" | sed -e 's/ ,/,/g')
+  content=$(echo "$content" | sed -e 's/, /,/g')
+  content=$(echo "$content" | sed -e 's/ {/{/g')
+  content=$(echo "$content" | sed -e 's/{ /{/g')
+  content=$(echo "$content" | sed -e 's/ }/}/g')
+  content=$(echo "$content" | sed -e 's/} /}/g')
+  content=$(echo "$content" | sed -e 's/ ;/;/g')
+  content=$(echo "$content" | sed -e 's/; /;/g')
+  content=$(echo "$content" | sed -e 's/ :/:/g')
+  content=$(echo "$content" | sed -e 's/: /:/g')
 
-(echo "$pre$content$post" > element-skeleton.html && echo "Build Successful") || echo "Build Failed";
-size=$(wc -c element-skeleton.html | sed -e "s/element-skeleton.html/ Bytes/g")
-echo "element-skeleton.html $size"
+  # export
+  echo "$content" > element-skeleton.min.css
+  pre="<dom-module id='element-skeleton'><template><style>"
+  post="</style></template></dom-module>"
+  echo "$pre$content$post" > element-skeleton.html
+
+  #generate reports
+  echo "LAST BUILD: $(date)"
+  size=$(wc -c element-skeleton.css | sed -e "s/element-skeleton.css/ Bytes/g")
+  echo -e "\telement-skeleton.css    \t$size"
+  size=$(wc -c element-skeleton.min.css | sed -e "s/element-skeleton.min.css/ Bytes/g")
+  echo -e "\telement-skeleton.min.css\t$size"
+  size=$(wc -c element-skeleton.html | sed -e "s/element-skeleton.html/ Bytes/g")
+  echo -e "\telement-skeleton.html   \t$size"
+}
+
+# record and report
+build_components > last_build.log
+cat last_build.log
